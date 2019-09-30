247cb39d9ddb9ff56a5bde5335732455457f0968
steven <steven@nenyah.com>
2019-09-30 12:21:59 +0800

    delete remote .tea

--- .tea/entryFiles-production/index$.web.js
+++
@@ -1,8 +0,0 @@
-require('@alipay/appx-compiler/lib/sjsEnvInit');
-require('./config$');
-
-require('../../pages/login/login');
-require('../../pages/index/index');
-require('../../pages/test/test');
-require('../../pages/about/about');
-require('../../pages/about/about');
