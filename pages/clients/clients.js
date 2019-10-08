b7aec903b8df1353db8bd3ea73c4f5715377077a
steven <steven@nenyah.com>
2019-10-08 16:33:20 +0800

    add clients page

---
+++ pages/clients/clients.js
@@ -0,0 +1,21 @@
+/*
+ * @Description: 
+ * @Author: Steven
+ * @Date: 2019-10-08 15:44:06
+ * @LastEditors: Steven
+ * @LastEditTime: 2019-10-08 16:16:10
+ */
+Page({
+  data: {
+    clients: [
+      { 'id': 1, 'name': '宁波第一医院' },
+      { 'id': 2, 'name': '宁波第二医院' },
+      { 'id': 3, 'name': '宁波第三医院' },
+      { 'id': 4, 'name': '宁波第四医院' },
+      { 'id': 5, 'name': '宁波第五医院' },
+      { 'id': 6, 'name': '宁波第六医院' },
+      { 'id': 7, 'name': '宁波第七医院' },
+    ]
+  },
+  onLoad() { },
+});
