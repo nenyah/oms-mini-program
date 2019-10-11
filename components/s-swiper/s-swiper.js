06eb895f814e5df6b7447f044bfa08a366d1c494
steven <steven@nenyah.com>
2019-10-11 15:04:50 +0800

    add s-swiper components

---
+++ components/s-swiper/s-swiper.js
@@ -0,0 +1,16 @@
+Component({
+  mixins: [],
+  data: {
+
+  },
+  props: {
+    imgUrls:{
+      type:Array,
+      value:[]
+    }
+  },
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+  methods: {},
+});
