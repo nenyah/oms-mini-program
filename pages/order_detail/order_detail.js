2e77e5b53c16df6ac0a4413df9ff6f7a3c0c362f
steven <steven@nenyah.com>
2019-10-17 15:20:24 +0800

    finish date picker function in order detail page

--- pages/order_detail/order_detail.js
+++ pages/order_detail/order_detail.js
@@ -5,6 +5,7 @@
  * @LastEditors: Steven
  * @LastEditTime: 2019-10-09 16:28:05
  */
+import utils from '/utils/utils.js'
 Page({
   data: {
     list: [{ //商品列表
@@ -26,6 +27,24 @@ Page({
       select: false
     }],
     address: { 'mobile': '159 **** 2694', 'name': '王天霸', 'city': ['浙江', '宁波', '北仑'], 'detailed': '大碶街道不知路不知号' },
+    deliver_date:"",
+    pay_date:"",
   },
   onLoad() { },
+  _setDate(date_cate) {
+    let today = utils.formatDate(new Date(),'-')
+    dd.datePicker({
+      format: 'yyyy-MM-dd',
+      currentDate: today,
+      success: (res) => {
+        this.setData({
+          [date_cate]:res.date,
+        })
+      },
+    })
+  },
+  choose_date(event){
+    let date_cate = event.currentTarget.dataset.dateCate
+    this._setDate(date_cate)
+  },
 });
